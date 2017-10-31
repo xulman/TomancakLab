fa17e41653eb8956fabaae76cf3cb1c2c8cb6f66
Vladimír Ulman <ulman@mpi-cbg.de>
2017-10-31 08:33:58 +0100

    ADD: routine to read in a Matlab-ImSAnE-exported text image with pixel areas

---
+++ areaInvolved_MF/realAreas.py
@@ -0,0 +1,25 @@
+from ij import IJ
+import sys
+
+def readRealSizes():
+
+	AreaFile = IJ.getFilePath("Choose your pixel areas, Matlab exported file:")
+	if (AreaFile is None):
+			sys.exit('User canceled Dialog!')
+
+	# open the selected file
+	IJ.run("Text Image... ", "open="+AreaFile);
+
+	# get pixel 1D array, and image sizes
+	imp = IJ.getImage();
+	rawPixels = imp.getProcessor().getPixels();
+	hh = imp.height;
+	ww = imp.width;
+
+	# convert the data array to "2D image", and close it
+	realSizes = [[rawPixels[y*ww +x] for y in range(hh)] for x in range(ww)]
+	imp.close();
+
+	# and return the "2D pixel areas"
+	return realSizes;
+
