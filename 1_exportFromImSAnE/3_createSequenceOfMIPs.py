2235cb8fc80f26958bed3308bd0851ff274109fd
Vladimír Ulman <ulman@mpi-cbg.de>
2017-12-12 14:54:04 +0100

    ADD: 1_folder/1_createSequenceOfMIPs -- initial not-tested script

---
+++ 1_exportFromImSAnE/3_createSequenceOfMIPs.py
@@ -0,0 +1,95 @@
+#@File (style="directory", label="Directory with the stacks") inputDir
+#@int (label="First time-point to be used", value="1") timeSpanFrom
+#@int (label="Last  time-point to be used", value="2") timeSpanTo
+#@int (label="MIP A: z-slice FROM", value="0") AsliceFrom
+#@int (label="MIP A: z-slice   TO", value="1") AsliceTo
+#@int (label="MIP B: z-slice FROM", value="0") BsliceFrom
+#@int (label="MIP B: z-slice   TO", value="1") BsliceTo
+
+# This script works on the output of the previous script, that is it works on a
+# sequence of 3D stacks that are to be found in the folder $inputDir. It then
+# reads one by one the stacks withint the given time point interval and extracts
+# Maximum Intensity Projections (MIPs) over the 3rd coordinate (z-axis). There
+# will be actually two such projections calculated, each over its interval of
+# z-slices, and saved into $inputDir/MIP_A and $inputDir/MIP_B, respectively.
+
+# Usage:
+# 	- Run Fiji
+#	- Run this script
+#	- Choose the input folder and ranges
+
+from ij import IJ
+import ij.ImagePlus
+import ij.ImageStack
+
+
+def main():
+	# check that input z-ranges make sense
+	if (AsliceFrom < 0 or BsliceFrom < 0):
+		print "A or B slice FROM index can't be negative."
+		return
+	if (AsliceFrom > AsliceTo or BsliceFrom > BsliceTo):
+		print "A or B slice FROM index can't be larger than TO index."
+		return
+
+	# a list of time points
+	for TT in range(timeSpanFrom,timeSpanTo+1):
+		# a properly formatted string with time point
+		T = '%(time)04d' % { 'time' : TT }
+
+		# read the input stack
+		F = inputDir.getPath() + "/cmp_3D_T" + T + ".tif"
+		img = IJ.openImage(F)
+		if (img is None):
+			print "couldn't open file: "+F
+			return
+
+		# check whether A/BsliceTo indices are within the image z-axis range
+		if (AsliceTo > img.getStackSize() or BsliceTo > img.getStackSize()):
+			print "A or B slice TO index is beyond last z-slice."
+			return
+
+		# create a zeroed yet-empty projected plane
+		plane = [[ 0 for y in range(img.getHeight()) ] for x in range(img.getWidth()) ]
+
+		# do the projection over the first range of slices
+		for Z in range(AsliceFrom, AsliceTo+1):
+			img.setZ(Z+1)
+			ip = img.getProcessor();
+
+			# one slice "merging"
+			for y in range(ip.height):
+				for x in range(ip.width):
+					val = ip.getf(x,y)
+					if (val > plane[x][y]):
+						plane[x][y] = val
+
+		# now, create and save the image
+		ip.setPixels(plane);
+		F = inputDir.getPath() + "MIP_A/cmp_MIP_T" + T + ".tif"
+		print "saving: "+F
+		IJ.save(ij.ImagePlus(F,ip),F)
+
+		# create a zeroed yet-empty projected plane
+		plane = [[ 0 for y in range(img.getHeight()) ] for x in range(img.getWidth()) ]
+
+		# do the projection over the first range of slices
+		for Z in range(BsliceFrom, BsliceTo+1):
+			img.setZ(Z+1)
+			ip = img.getProcessor();
+
+			# one slice "merging"
+			for y in range(ip.height):
+				for x in range(ip.width):
+					val = ip.getf(x,y)
+					if (val > plane[x][y]):
+						plane[x][y] = val
+
+		# now, create and save the image
+		ip.setPixels(plane);
+		F = inputDir.getPath() + "MIP_B/cmp_MIP_T" + T + ".tif"
+		print "saving: "+F
+		IJ.save(ij.ImagePlus(F,ip),F)
+
+
+main()
