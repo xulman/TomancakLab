9bf8edb62086941db78cfe0ee5f81493e933002c
Vladimír Ulman <ulman@mpi-cbg.de>
2017-12-16 08:50:30 +0100

    ADD: 1_folder/3_createMIPs now recognizes 4 "sub-bands"

--- 1_exportFromImSAnE/3_createSequenceOfMIPs.py
+++ 1_exportFromImSAnE/3_createSequenceOfMIPs.py
@@ -5,6 +5,10 @@
 #@int (label="MIP A: z-slice   TO", value="1") AsliceTo
 #@int (label="MIP B: z-slice FROM", value="0") BsliceFrom
 #@int (label="MIP B: z-slice   TO", value="1") BsliceTo
+#@int (label="MIP C: z-slice FROM", value="0") CsliceFrom
+#@int (label="MIP C: z-slice   TO", value="1") CsliceTo
+#@int (label="MIP D: z-slice FROM", value="0") DsliceFrom
+#@int (label="MIP D: z-slice   TO", value="1") DsliceTo
 
 # This script works on the output of the previous script, that is it works on a
 # sequence of 3D stacks that are to be found in the folder $inputDir. It then
@@ -32,12 +36,18 @@ def main():
 	if (os.path.isdir(inputDir.getPath() + "/MIP_B") == False):
 		os.mkdir(inputDir.getPath() + "/MIP_B")
 
+	if (os.path.isdir(inputDir.getPath() + "/MIP_C") == False):
+		os.mkdir(inputDir.getPath() + "/MIP_C")
+
+	if (os.path.isdir(inputDir.getPath() + "/MIP_D") == False):
+		os.mkdir(inputDir.getPath() + "/MIP_D")
+
 	# check that input z-ranges make sense
-	if (AsliceFrom < 1 or BsliceFrom < 1):
-		print "A or B slice FROM index can't be less than 1."
+	if (AsliceFrom < 1 or BsliceFrom < 1 or CsliceFrom < 1 or DsliceFrom < 1):
+		print "Some slice FROM index can't be less than 1."
 		return
-	if (AsliceFrom > AsliceTo or BsliceFrom > BsliceTo):
-		print "A or B slice FROM index can't be larger than TO index."
+	if (AsliceFrom > AsliceTo or BsliceFrom > BsliceTo or CsliceFrom > CsliceTo or DsliceFrom > DsliceTo):
+		print "Some slice FROM index can't be larger than TO index."
 		return
 
 	# a list of time points
@@ -70,6 +80,22 @@ def main():
 		IJ.run("Z Project...", "start="+str(BsliceFrom)+" stop="+str(BsliceTo)+" projection=[Max Intensity]");
 		omg = IJ.getImage();
 		F = inputDir.getPath() + "/MIP_B/cmp_MIP_T" + T + ".tif"
+		print "saving: "+F
+		IJ.save(omg,F)
+		omg.close()
+
+		# 3rd interval
+		IJ.run("Z Project...", "start="+str(CsliceFrom)+" stop="+str(CsliceTo)+" projection=[Max Intensity]");
+		omg = IJ.getImage();
+		F = inputDir.getPath() + "/MIP_C/cmp_MIP_T" + T + ".tif"
+		print "saving: "+F
+		IJ.save(omg,F)
+		omg.close()
+
+		# 4th interval
+		IJ.run("Z Project...", "start="+str(DsliceFrom)+" stop="+str(DsliceTo)+" projection=[Max Intensity]");
+		omg = IJ.getImage();
+		F = inputDir.getPath() + "/MIP_D/cmp_MIP_T" + T + ".tif"
 		print "saving: "+F
 		IJ.save(omg,F)
 		omg.close()
