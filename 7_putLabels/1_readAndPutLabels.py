3f163c69845e6e02453fefd44916d1a1735421b2
Vladimír Ulman <ulman@mpi-cbg.de>
2018-07-03 17:04:22 +0200

    ADD,CHG, 7/1: optional fixed text is added, renders text only within range of existing slices

--- 7_putLabels/1_readAndPutLabels.py
+++ 7_putLabels/1_readAndPutLabels.py
@@ -1,10 +1,15 @@
 #@ boolean (label="Double click 'TextTool' to choose font") notUsed1
 #@ boolean (label="Double click 'ColorPicker' to choose color") notUsed2
-#@ int (label="bottom left corner [pixels], X=") X
-#@ int (label="bottom left corner [pixels], Y=") Y
-#@ String (label="prefix text=", required=False) labelsPrefix
+#@ int (label="label's bottom left corner [pixels], X=") X
+#@ int (label="label's bottom left corner [pixels], Y=") Y
+#@ String (label="label prefix=", required=False) labelsPrefix
 #@ File (label="file with labels") labelsFile
-#@ String (label="postfix text=", required=False) labelsPostfix
+#@ String (label="label postfix=", required=False) labelsPostfix
+
+#@ int (label="text's bottom left corner [pixels], X=") Xt
+#@ int (label="text's bottom left corner [pixels], Y=") Yt
+#@ String (label="text itself=", required=False) textMsg
+
 labelsFile = labelsFile.getAbsolutePath()
 if labelsPrefix == None:
 	labelsPrefix = ""
@@ -66,21 +71,31 @@ image = IJ.getImage()
 labels = readLabelsTxtFile(labelsFile)
 
 def main():
-	# sweep through the image stack
+	# sweep through the time points from the file
 	for l in range(len(labels)):
-		image.setSlice(labels[l][0])
+		z = labels[l][0]
 
-		# set font-info for the current slice
-		ip = image.getProcessor()
-		ip.setFont(font)
-		ip.setColor(color)
+		# check the time point is within the range of the opened image
+		if z > 0 and z <= image.getStackSize():
+			image.setSlice(z)
 
-		# render the text to the current slice
-		ip.moveTo(X, Y)
-		msg = labelsPrefix+str(labels[l][1])+labelsPostfix
+			# set font-info for the current slice
+			ip = image.getProcessor()
+			ip.setFont(font)
+			ip.setColor(color)
 
-		ip.drawString(msg)
-		print "drawing >"+msg+"< into slice >"+str(labels[l][0])+"<"
+			# render the text to the current slice
+			ip.moveTo(X, Y)
+			msg = labelsPrefix+str(labels[l][1])+labelsPostfix
+
+			ip.drawString(msg)
+			print "drawing >"+msg+"< into slice >"+str(z)+"<"
+
+			# draw also the fixed additional text/annotation
+			if textMsg != None and len(textMsg) > 0:
+				ip.moveTo(Xt,Yt)
+				ip.drawString(textMsg)
+				print "drawing >"+textMsg+"< into slice >"+str(z)+"<"
 
 
 main()
