d0160ee614aef92400ca2e27f68d26833ae35dcc
Vladimír Ulman <ulman@mpi-cbg.de>
2018-09-12 19:51:57 +0200

    ADD: */0_intro intro menu item with "where to get help" pointers

---
+++ 1_exportFromImSAnE/0_introduction.py
@@ -0,0 +1,4 @@
+from ij import IJ
+IJ.showMessage("Where to get help","Please visit this web page:\n \n"
+              +"https://github.com/xulman/TomancakLab.git\n \n"
+              +"and click on '1_exportFromImSAnE' folder in the list of folders.")
---
+++ 5_mamuth/0_introduction.py
@@ -0,0 +1,4 @@
+from ij import IJ
+IJ.showMessage("Where to get help","Please visit this web page:\n \n"
+              +"https://github.com/xulman/TomancakLab.git\n \n"
+              +"and click on '5_mamuth' folder in the list of folders.")
