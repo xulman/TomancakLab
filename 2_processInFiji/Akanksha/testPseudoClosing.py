19802f2f8a4413aed02ca0be9f1212b94f3bda1d
Vladimír Ulman <ulman@mpi-cbg.de>
2018-12-18 21:19:16 +0100

    add pseudoDilation code

--- 2_processInFiji/Akanksha/testPseudoClosing.py
+++ 2_processInFiji/Akanksha/testPseudoClosing.py
@@ -58,6 +58,47 @@ def pseudoClosing(imp):
 				if len(v) == 2 and v[0] == 0 and h[v[1]] > 4:
 					i[o] = v[1]
 					countChanges = countChanges+1
+				if len(v) == 2 and v[1] == 0 and h[v[0]] > 4:
+					i[o] = v[0]
+					countChanges = countChanges+1
+
+	return countChanges
+
+
+def pseudoDilation(imp):
+	i = imp.getProcessor().getPixels()
+	w = imp.getWidth()
+	h = imp.getHeight()
+
+	# have a copy of the original so that "neighborhood computations"
+	# are not affected by the on-going processing
+	I = [ i[o] for o in range(len(i)) ]
+
+	countChanges = 0
+
+	for y in range(1,h-1):
+		for x in range(1,w-1):
+			# offset
+			o = y*w + x
+
+			if I[o] == 0:
+				# found empty/black pixel,
+				# is it surrounded only by the same non-zero colour?
+				h = findUniqueNeighbors(I,o,w,h)
+
+				v = h.keys()
+				# one-bin histogram: surrounded by fg voxels?
+				if len(v) == 1 and v[0] > 0:
+					i[o] = v[0]
+					countChanges = countChanges+1
+
+				# two-bins histogram: surrounded by bg and by single-fg voxels?
+				if len(v) == 2 and v[0] == 0:
+					i[o] = v[1]
+					countChanges = countChanges+1
+				if len(v) == 2 and v[1] == 0:
+					i[o] = v[0]
+					countChanges = countChanges+1
 
 	return countChanges
 
@@ -89,7 +130,9 @@ def pluginCode():
 		if cnt <= minPixelChanges:
 			break;
 
-	print "finito."
+	cnt = pseudoDilation(imp)
+	imp.updateAndRepaintWindow()
+	print "finito with final dilation, saw "+str(cnt)+" changes"
 
 
 pluginCode()
