9e400bf0f8800f92aded039f9228ad1d929ee21e
Vladimír Ulman <ulman@mpi-cbg.de>
2019-06-27 17:31:18 +0200

    ADD: skeleton of the Open&Send to SimViewer

---
+++ Mastodon/src/main/java/de/mpicbg/tomancaklab/OpenSimViewerAndSendTracking.java
@@ -0,0 +1,136 @@
+package de.mpicbg.tomancaklab;
+
+import static org.mastodon.app.ui.ViewMenuBuilder.item;
+import static org.mastodon.app.ui.ViewMenuBuilder.menu;
+
+import org.mastodon.app.ui.ViewMenuBuilder;
+import org.mastodon.plugin.MastodonPlugin;
+import org.mastodon.plugin.MastodonPluginAppModel;
+
+import org.mastodon.project.MamutProject;
+import org.mastodon.revised.mamut.Mastodon;
+import org.mastodon.revised.mamut.WindowManager;
+import org.mastodon.spatial.SpatioTemporalIndex;
+import org.mastodon.revised.mamut.MamutAppModel;
+import org.mastodon.revised.model.mamut.Model;
+import org.mastodon.revised.model.mamut.ModelGraph;
+import org.mastodon.revised.model.mamut.Spot;
+import org.mastodon.revised.model.mamut.Link;
+
+import org.scijava.AbstractContextual;
+import org.scijava.Context;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.behaviour.util.Actions;
+import org.scijava.ui.behaviour.util.AbstractNamedAction;
+import org.scijava.ui.behaviour.util.RunnableAction;
+import org.scijava.log.LogService;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.*;
+
+
+@Plugin( type = OpenSimViewerAndSendTracking.class )
+public class OpenSimViewerAndSendTracking extends AbstractContextual implements MastodonPlugin
+{
+	//"IDs" of all plug-ins wrapped in this class
+	private static final String PluginID_open = "LoPaT-OpenSimViewer";
+	private static final String PluginID_send = "LoPaT-SendTracking";
+	//------------------------------------------------------------------------
+
+	@Override
+	public List< ViewMenuBuilder.MenuItem > getMenuItems()
+	{
+		//this places the plug-in's menu items into the menu,
+		//the titles of the items are defined right below
+		return Arrays.asList(
+				menu( "Plugins",
+						item( PluginID_open ), item ( PluginID_send ) ) );
+	}
+
+	/** titles of this plug-in's menu items */
+	private static Map< String, String > menuTexts = new HashMap<>();
+	static
+	{
+		menuTexts.put( PluginID_open, "Open SimViewer" );
+		menuTexts.put( PluginID_send, "Populate SimViewer" );
+	}
+
+	@Override
+	public Map< String, String > getMenuTexts()
+	{
+		return menuTexts;
+	}
+	//------------------------------------------------------------------------
+
+	private final AbstractNamedAction actionOpen;
+	private final AbstractNamedAction actionSend;
+
+	/** default c'tor: creates Actions available from this plug-in */
+	public OpenSimViewerAndSendTracking()
+	{
+		actionOpen = new RunnableAction( PluginID_open, this::workerOpen );
+		actionSend = new RunnableAction( PluginID_send, this::workerSend );
+		updateEnabledActions();
+	}
+
+	/** register the actions to the application (with no shortcut keys) */
+	@Override
+	public void installGlobalActions( final Actions actions )
+	{
+		final String[] noShortCut = new String[] {};
+		actions.namedAction( actionOpen, noShortCut );
+		actions.namedAction( actionSend, noShortCut );
+	}
+
+	/** reference to the currently available project in Mastodon */
+	private MastodonPluginAppModel pluginAppModel;
+
+	/** learn about the current project's params */
+	@Override
+	public void setAppModel( final MastodonPluginAppModel model )
+	{
+		//the application reports back to us if some project is available
+		this.pluginAppModel = model;
+		updateEnabledActions();
+	}
+
+	/** enables/disables menu items based on the availability of some project */
+	private void updateEnabledActions()
+	{
+		final MamutAppModel appModel = ( pluginAppModel == null ) ? null : pluginAppModel.getAppModel();
+		actionOpen.setEnabled( appModel != null );
+		actionSend.setEnabled( appModel != null );
+	}
+	//------------------------------------------------------------------------
+	private LogService logServiceRef; //(re)defined with every call to this.workerSRES()
+
+
+	/** opens the SimViewer */
+	private void workerOpen()
+	{
+		System.out.println("open SimViewer");
+	}
+
+	/** sends some (now fake!) stuff to the SimViewer */
+	private void workerSend()
+	{
+		System.out.println("populate SimViewer");
+	}
+
+
+	public static void main( final String[] args ) throws Exception
+	{
+		Locale.setDefault( Locale.US );
+		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+
+		final MamutProject project = new MamutProject( null, new File( "x=1000 y=1000 z=100 sx=1 sy=1 sz=10 t=400.dummy" ) );
+//		final MamutProject project = new MamutProjectIO().load( "/Users/ulman/DATA/CTC2/T_carto_forMastodon/corrections/" );
+
+		final Mastodon mastodon = new Mastodon();
+		new Context().inject( mastodon );
+		mastodon.run();
+		mastodon.setExitOnClose();
+		mastodon.openProject( project );
+	}
+}
