375f1df79bd5e052e51475f4a2dfa8e0683013d5
Vladimír Ulman <ulman@mpi-cbg.de>
2019-07-11 02:26:56 +0200

    CHG,FIX: yEd graph exported implements the GraphExportable interface

--- Mastodon/src/main/java/de/mpicbg/tomancaklab/graphexport/yEdGraphMLWriter.java
+++ Mastodon/src/main/java/de/mpicbg/tomancaklab/graphexport/yEdGraphMLWriter.java
@@ -1,11 +1,10 @@
 package de.mpicbg.tomancaklab.graphexport;
 
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class yEdGraphMLWriter
+public class yEdGraphMLWriter implements GraphExportable
 {
 	// -----------------------------------------------------------------------------
 	BufferedWriter file;
@@ -27,6 +26,8 @@ public class yEdGraphMLWriter
 	}
 
 	boolean closed = false;
+
+	@Override
 	public void close()
 	{
 		try {
@@ -76,6 +77,7 @@ public class yEdGraphMLWriter
 		addNode(id, label,colorRGB, 0,0, defaultNodeWidth,defaultNodeHeight);
 	}
 
+	@Override
 	public void addNode(final String id,
 	             final String label, final int colorRGB,
 	             final int x, final int y)
@@ -83,6 +85,7 @@ public class yEdGraphMLWriter
 		addNode(id, label,colorRGB, x,y, defaultNodeWidth,defaultNodeHeight);
 	}
 
+	@Override
 	public void addNode(final String id,
 	             final String label, final int colorRGB,
 	             final int x, final int y,
@@ -148,38 +151,47 @@ public class yEdGraphMLWriter
 	}
 	// -----------------------------------------------------------------------------
 
+	@Override
 	public void addStraightLine(final String fromId, final String toId)
 	{
 		writeEdge(fromId.concat(toId), fromId, toId);
 		System.out.println(fromId+" -> "+toId);
 	}
 
+	@Override
 	public void addStraightLineConnectedVertex(final String parentNodeID,
 	                                           final String newNodeID,
 	                                           final String label, final int colorRGB,
-	                                           final int... xyz)
+	                                           final int x, final int y)
 	{
-		addNode(newNodeID, label,colorRGB, xyz[0],xyz[1]);
+		addNode(newNodeID, label,colorRGB, x,y);
 		addStraightLine(parentNodeID, newNodeID);
 	}
 	// -----------------------------------------------------------------------------
 
-	final int bendingPointAbsoluteOffset = -80;
-
+	@Override
 	public void addBendedLine(final String fromId, final String toId,
 	                          final int toX, final int toY)
 	{
-		writeEdge(fromId.concat(toId), fromId, toId, toX+defaultNodeWidth/2,toY+bendingPointAbsoluteOffset);
+		addBendedLine(fromId,toId, toX,toY, defaultBendingPointAbsoluteOffsetY);
+	}
+
+	@Override
+	public void addBendedLine(final String fromId, final String toId,
+	                          final int toX, final int toY, final int bendingOffsetY)
+	{
+		writeEdge(fromId.concat(toId), fromId, toId, toX+defaultNodeWidth/2,toY+bendingOffsetY);
 		System.out.println(fromId+" -> "+toId);
 	}
 
+	@Override
 	public void addBendedLineConnectedVertex(final String parentNodeID,
 	                                         final String newNodeID,
 	                                         final String label, final int colorRGB,
-	                                         final int... xyz)
+	                                         final int x, final int y)
 	{
-		addNode(newNodeID, label,colorRGB, xyz[0],xyz[1]);
-		addBendedLine(parentNodeID, newNodeID, xyz[0],xyz[1]);
+		addNode(newNodeID, label,colorRGB, x,y);
+		addBendedLine(parentNodeID, newNodeID, x,y);
 	}
 	// -----------------------------------------------------------------------------
 
@@ -191,14 +203,14 @@ public class yEdGraphMLWriter
 		addNode("A", "A",defaultNodeColour, 200,0);
 
 		//left subtree: straight lines
-		addStraightLineConnectedVertex("A" , "AL" , "AL" ,defaultNodeColour, 100,200,0);
-		addStraightLineConnectedVertex("AL", "ALL", "ALL",defaultNodeColour,  50,400,0);
-		addStraightLineConnectedVertex("AL", "ALR", "ALR",defaultNodeColour, 150,400,0);
+		addStraightLineConnectedVertex("A" , "AL" , "AL" ,defaultNodeColour, 100,200);
+		addStraightLineConnectedVertex("AL", "ALL", "ALL",defaultNodeColour,  50,400);
+		addStraightLineConnectedVertex("AL", "ALR", "ALR",defaultNodeColour, 150,400);
 
 		//right subtree: bended lines
-		addBendedLineConnectedVertex( "A" , "AR" , "AR" ,defaultNodeColour, 300,200,0);
-		addBendedLineConnectedVertex( "AR", "ARL", "ARL",defaultNodeColour, 250,400,0);
-		addBendedLineConnectedVertex( "AR", "ARR", "ARR",defaultNodeColour, 350,400,0);
+		addBendedLineConnectedVertex( "A" , "AR" , "AR" ,defaultNodeColour, 300,200);
+		addBendedLineConnectedVertex( "AR", "ARL", "ARL",defaultNodeColour, 250,400);
+		addBendedLineConnectedVertex( "AR", "ARR", "ARR",defaultNodeColour, 350,400);
 
 		System.out.println("GraphML stopped");
 	}
